4e4c24473464aad3d0dc327f21b7c477b4d071f4
Xander Moffatt <xmoffatt@instructure.com>
2017-08-21 13:20:04 -0600

    add markup! yay

--- main.elm
+++ main.elm
@@ -49,8 +49,14 @@ updateGPA model =
 
     totalPoints =
       List.foldr (+) model.initialPoints points
+
+    gpa =
+      if totalHours == 0 then
+        0.0
+      else
+        totalPoints / totalHours
   in
-    { model | projectedGPA = totalPoints / totalHours }
+    { model | projectedGPA = gpa }
 
 resetCurrentClass: Model -> Model
 resetCurrentClass model =
@@ -84,6 +90,7 @@ model =
 
 type Msg
   = AddClass
+  | RemoveClass Int
   | InitialHours String
   | InitialPoints String
   | ClassName String
@@ -97,9 +104,23 @@ update msg model =
   in
     case msg of
       AddClass ->
-          { model | classes = model.currentClass :: model.classes }
+        { model | classes = model.currentClass :: model.classes }
+        |> updateGPA
+        |> resetCurrentClass
+
+      RemoveClass index ->
+        let
+            before =
+              List.take index model.classes
+
+            after =
+              List.drop (index + 1) model.classes
+
+            newClasses =
+              before ++ after
+        in
+          {model | classes = newClasses }
           |> updateGPA
-          |> resetCurrentClass
 
       InitialHours hours ->
         { model | initialHours = Result.withDefault 0 (String.toFloat hours) }
@@ -137,40 +158,100 @@ update msg model =
 
 view: Model -> Html Msg
 view model =
-  div []
-    [ initialData model
+  div [ class "container" ]
+    [ node "link" [ rel "stylesheet", href "style.css" ] []
+    , node "link" [ rel "stylesheet", type_ "text/css", href "//fonts.googleapis.com/css?family=Raleway:400,300,600"] []
+    , heading model
+    , initialData model
+    , classInfo model
     , newClass model
-    , ul [] (List.map viewClass model.classes)
-    , div [] [ text (Round.round 2 model.projectedGPA) ]
+    , classTable model
+    , projectedGPA model
     ]
 
+heading: Model -> Html msg
+heading model =
+  div [ class "heading row" ]
+    [
+      div [ class "offset-by-three six columns"]
+        [ h1 [ class "centered" ] [ text "GPA Calculator" ]
+        , h6 [ class "centered" ] [ text "A simple cumulative GPA calculator, built entirely in Elm. The GPA calculator is my personal \"Hello World\" that lets me experience the language enough to enjoy it. CSS provided by Skeleton."]
+        ]
+    ]
 initialData: Model -> Html Msg
 initialData model =
-  div []
-    [ textInput "Initial Credit Hours" InitialHours (toEmptyString model.initialHours)
-    , textInput "Initial Points" InitialPoints (toEmptyString model.initialPoints)
+  div [ class "row" ]
+    [  step 1
+    ,  description "(Optional) Add your current points and credit hours. These are usually found on your transcript or class record."
+    , div [ class "six columns" ]
+      [ textInput "Initial Credit Hours" InitialHours (toEmptyString model.initialHours)
+      , textInput "Initial Points" InitialPoints (toEmptyString model.initialPoints)
+      ]
+    ]
+
+classInfo: Model -> Html Msg
+classInfo model =
+  div [ class "row" ]
+    [ step 2
+    , description "Add desired classes - the name is optional, but hours and a standard letter grade are required."
     ]
 
 newClass: Model -> Html Msg
 newClass model =
-  div []
-    [ textInput "Class Name" ClassName model.currentClass.name
-    , textInput "Class Hours" ClassHours (toEmptyString model.currentClass.hours)
-    , textInput "Class Grade" ClassGrade model.currentClass.grade
-    , button [ onClick AddClass ] [ text "Add Class" ]
+  div [ class "row" ]
+    [
+      div [ class "offset-by-two columns" ]
+        [ textInput "Class Name" ClassName model.currentClass.name
+        , textInput "Class Hours" ClassHours (toEmptyString model.currentClass.hours)
+        , textInput "Class Grade" ClassGrade model.currentClass.grade
+        , button [ onClick AddClass, class "button-primary" ] [ text "Add Class" ]
+        ]
     ]
 
-viewClass: Class -> Html Msg
-viewClass class =
-  li []
-    [ div [] [ text class.name ]
-    , div [] [ text class.grade ]
-    , div [] [ text (Round.round 1 class.hours) ]
+classTable: Model -> Html Msg
+classTable model =
+  div [ class "row" ]
+    [
+      div [ class "offset-by-two ten columns"]
+       [
+        ol [] (List.indexedMap viewClass model.classes)
+       ]
     ]
+
+viewClass: Int -> Class -> Html Msg
+viewClass index currentClass =
+  li []
+    [ readOnlyInput currentClass.name
+    , readOnlyInput (Round.round 1 currentClass.hours)
+    , readOnlyInput currentClass.grade
+    , button [ onClick (RemoveClass index) ] [ text "Remove" ]
+    ]
+
+projectedGPA: Model -> Html Msg
+projectedGPA model =
+  div [ class "row" ]
+    [ step 3
+    , description "Check your projected GPA (and try not to freak out about the classes where you need a better grade)."
+    , div [ class "six columns" ] [ h2 [] [ text (Round.round 2 model.projectedGPA) ] ]
+    ]
+
+-- helper view functions
+
+step: Int -> Html Msg
+step num =
+  div [ class "two columns" ] [ h4 [] [ text ("Step " ++ (toString num) ++ ":") ] ]
+
+description: String -> Html Msg
+description blurb =
+  div [ class "four columns" ] [ text blurb ]
+
+readOnlyInput: String -> Html Msg
+readOnlyInput v =
+  input [ class "class-info", type_ "text", value v, disabled True ] []
 
 textInput: String -> (String -> Msg) -> String -> Html Msg
 textInput name msg v  =
-  input [ type_ "text" , placeholder name, onBlurWithTargetValue msg, value v ] []
+  input [ type_ "text" , placeholder name, onBlurWithTargetValue msg, value v, class "text-input" ] []
 
 onBlurWithTargetValue : (String -> msg) -> Attribute msg
 onBlurWithTargetValue tagger =
